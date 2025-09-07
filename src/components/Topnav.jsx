eba08b9b9775a8b7ae9b62ade9b2ee208da72107
sodaaw <ghk132435@naver.com>
2025-09-08 00:44:57 +0900

    topnav 수정

--- src/components/Topnav.jsx
+++ src/components/Topnav.jsx
@@ -57,14 +57,14 @@ export default function Topnav({ variant = "default" }) {
           {/* Main.jsx가 아닌 경우에만 검색 아이콘 표시 */}
           {!isHome && (
             <span
-              className="search-icon"
+              className="nav-search-icon"
               role="button"
               tabIndex={0}
               aria-label="검색"
               onClick={openSearchModal}
               onKeyDown={(e) => e.key === 'Enter' && openSearchModal()}
             >
-              <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <circle cx="11" cy="11" r="8" stroke="currentColor" strokeWidth="2" fill="none"/>
                 <path d="m21 21-4.35-4.35" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
               </svg>
@@ -90,7 +90,7 @@ export default function Topnav({ variant = "default" }) {
         {/* 오른쪽 영역 */}
         <div className="topnav-right">
           <button 
-            className="login-btn" 
+            className="topnav-login-btn" 
             onClick={() => navigate('/login')}
             aria-label="로그인"
           >
